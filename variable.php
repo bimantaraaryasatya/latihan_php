eae445d72d444deef96f1f29b0bf39f72fc84d47
bimantaraaryasatya <bimantara_aryasatya_32rpl@student.smktelkom-mlg.sch.id>
2024-08-06 07:00:03 +0700

    variable php

---
+++ variable.php
@@ -0,0 +1,19 @@
+<?php
+    $nama = "Bima"; // String
+    $kelas = "XI RPL 4"; 
+    $umur = 16; // Integer
+    $uangJajan = 255.5; // float / double
+    $statusPelajar = true; // boolean
+
+    // print variable
+    echo $nama;
+    echo "<br>";
+    echo $kelas;
+    echo "<br>";
+    echo $umur;
+    echo "<br>";
+    echo $uangJajan;
+    echo "<br>";
+    echo "Status Pelajar: "; 
+    var_dump($statusPelajar);
+?>
